4b01486fc624c9a69d0559bb67a18618a89c2907
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-21 18:03:29 +1000

    chore: Add rules page

--- src/components/NavBar.svelte
+++ src/components/NavBar.svelte
@@ -1,6 +1,17 @@
-<div class='flex flex-row items-center justify-center w-screen gap-4 pt-5 pl-3 md:justify-start'>
-  <a href='/' class='text-lg rounded-full md:text-xl btn-nav-main'>Server Features</a>
-  <a href='/' class='text-lg rounded-full md:text-xl btn-nav-main'>Server Rules</a>
+<div
+  class="flex flex-row items-center justify-center w-screen gap-4 pt-5 pl-3 md:justify-start"
+>
+  {#if window.location.pathname != '/'}<a
+      href="/"
+      class="text-lg rounded-full md:text-xl btn-nav-main">Home</a
+    >
+  {/if}
+  <a href="/" class="text-lg rounded-full md:text-xl btn-nav-main"
+    >Server Features</a
+  >
+  <a href="/" class="text-lg rounded-full md:text-xl btn-nav-main"
+    >Server Rules</a
+  >
 </div>
 
 <style>
@@ -8,6 +19,6 @@
     border-width: 3px;
     border-color: white;
 
-    padding: 5px;
+    padding: 6px;
   }
 </style>
