74d54d0d7a5d7d7ced43c3bad792cf1e72881ff6
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-21 18:25:04 +1000

    chore: Add footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,10 @@
+<div class='flex flex-col justify-center w-screen h-[5vh] items-center'>
+  <p>OmegaSCP&copy; 2021 - Present</p>
+  <p>SCP: Secret Laboratory created by Northwood Studios&trade; is not affiliated with this site.</p>
+</div>
+
+<style>
+  * {
+    margin: 0;
+  }
+</style>
