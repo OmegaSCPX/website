0b32eab1256461caa701261a519a9bed8903eaa3
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2023-05-21 17:34:57 +1000

    chore: Add NavBar

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,13 @@
+<div class='flex flex-row items-center justify-start w-screen gap-4 pt-5 pl-3'>
+  <a href='/' class='text-lg rounded-full btn-nav-main'>Server Features</a>
+  <a href='/' class='text-lg rounded-full btn-nav-main'>Server Rules</a>
+</div>
+
+<style>
+  div a {
+    border-width: 3px;
+    border-color: white;
+
+    padding: 5px;
+  }
+</style>
